11285d518611c34fca7ec7782ca083b11dadbfe0
root <root@ip-172-31-88-91.ec2.internal>
2025-01-26 20:40:34 +0000

    Checking if adding timeout helps here

--- frontend/src/components/ChatWindow.js
+++ frontend/src/components/ChatWindow.js
@@ -5,6 +5,7 @@ import '../ChatWindow.css';
 
 function ChatWindow({ chat }) {
   const [messages, setMessages] = useState([]);
+  const axiosInstance = axios.create({ timeout: 600000 });
 
   useEffect(() => {
     if (!chat) {
@@ -12,7 +13,7 @@ function ChatWindow({ chat }) {
       return;
     }
     // axios.get(`http://34.236.217.66:30009/chats/${chat.chat_id}/messages`, {
-      axios.get(`https://summarize-ai.com/api/chats/${chat.chat_id}/messages`, {
+      axiosInstance.get(`https://summarize-ai.com/api/chats/${chat.chat_id}/messages`, {
       withCredentials: true,
     })
     .then(res => setMessages(res.data))
@@ -24,7 +25,7 @@ function ChatWindow({ chat }) {
     try 
       {
         // const userMsg = await axios.post(`http://34.236.217.66:30009/chats/${chat.chat_id}/messages`,
-        const userMsg = await axios.post(`https://summarize-ai.com/api/chats/${chat.chat_id}/messages`,
+        const userMsg = await axiosInstance.post(`https://summarize-ai.com/api/chats/${chat.chat_id}/messages`,
           { userText: text }, 
           { withCredentials: true }
         );
